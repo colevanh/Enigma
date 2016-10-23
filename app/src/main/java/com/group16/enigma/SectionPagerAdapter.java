22902e207f8b08c75dfdd113bcb42f5a76c90566
hd910 <hd910@outlook.com>
2016-10-22 22:17:59 -0700

    Added tab layout

---
+++ app/src/main/java/com/group16/enigma/SectionPagerAdapter.java
@@ -0,0 +1,51 @@
+package com.group16.enigma;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Hayde on 22-Oct-16.
+ */
+
+
+public class SectionPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new Fragment();
+            case 1:
+                return new ChatListFragment();
+            case 2:
+                return new Fragment();
+            default:
+                //return new SecondTabFragment();
+                return new Fragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Friends";
+            case 1:
+                return "Chat";
+            case 2:
+                return "Settings";
+            default:
+                return "";
+        }
+    }
+}
