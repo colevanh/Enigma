b0063387ffa7151e4a5e04fb3075beeb5dbb7c10
Michaella Sheng <msheng@ucsd.edu>
2016-11-07 10:46:05 -0800

    Message

---
+++ app/src/main/java/com/group16/enigma/MessageDatabase.java
@@ -0,0 +1,10 @@
+package com.group16.enigma;
+
+/**
+ * Created by Michaella on 11/4/2016.
+ */
+
+import com.google.firebase.messaging.FirebaseMessaging;
+public class MessageDatabase {
+    private static final FirebaseMessaging sRef = new FirebaseMessaging("Enigma");
+}
