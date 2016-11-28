605758d39da0bb92f06598e9836382d16c52ab62
hd910 <hd910@outlook.com>
2016-11-28 10:45:16 -0800

    Populate settings page

--- app/src/main/java/com/group16/enigma/SettingsFragment.java
+++ app/src/main/java/com/group16/enigma/SettingsFragment.java
@@ -1,7 +1,11 @@
 package com.group16.enigma;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +28,18 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        Button github = (Button) v.findViewById(R.id.githubButton);
+        github.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/colevanh/Enigma"));
+                startActivity(browserIntent);
+            }
+        });
+
         return v;
+
     }
 }
